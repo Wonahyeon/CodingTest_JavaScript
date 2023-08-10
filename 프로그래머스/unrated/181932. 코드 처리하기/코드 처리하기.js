e43a52bcb225cc76072e71d8b83a49bab3f8fc4f
Wonahyeon <49190965+Wonahyeon@users.noreply.github.com>
2023-08-10 19:57:48 +0900

    [unrated] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,23 +1,14 @@
 function solution(code) {
-    var answer = '';
+     let answer = '';
     let mode = 0;
-    let ret = '';
-    
-    for(let idx = 0; idx < code.length; idx++) {
-        if(mode === 0) {
-            if(code[idx] !== "1" && idx % 2 === 0) {
-                ret += code[idx];
-            } else if(code[idx] === "1") mode = 1; 
-        } else {
-            if(code[idx] !== "1" && idx % 2 === 1) {
-                ret += code[idx];
-            } else if(code[idx] === "1") mode = 0;
-        }
+
+    for (let i = 0; i < code.length; i += 1) {
+      if (Number(code[i]) === 1) {
+        mode = mode === 1 ? 0 : 1;
+      }
+      if (Number(code[i]) !== 1 && i % 2 === mode) {
+        answer += code[i];
+      }
     }
-    if(ret) {
-        answer = ret;
-    } else {
-        answer = "EMPTY";
-    }
-    return answer;
+    return answer.length > 0 ? answer : 'EMPTY';
 }
