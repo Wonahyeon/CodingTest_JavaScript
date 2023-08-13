54314d91d24a59704904f4f2383e79c7f1abf2fb
Wonahyeon <49190965+Wonahyeon@users.noreply.github.com>
2023-08-14 00:22:25 +0900

    [unrated] Title: 수 조작하기 1, Time: 2.66 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,21 @@
+function solution(n, control) {
+    var answer = 0;
+    for(let i = 0; i < control.length; i++){
+        switch(control[i]){
+            case "w":
+                n += 1;
+                break;
+            case "s":
+                n -= 1;
+                break;
+            case "d":
+                n += 10;
+                break;
+            case "a":
+                n -= 10;
+                break;
+        }
+    }
+    answer = n;
+    return answer;
+}
